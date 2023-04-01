8c43c1d008fde17d7e8c596f2df46edb1e2c0145
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-04-01 18:33:17 +0000

    initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import Header from '../Header/Header'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="left-h">
+            <Header/>
+        </div>
+        <div className="right-h"></div>
+
+    </div>
+  )
+}
+
+export default Hero
