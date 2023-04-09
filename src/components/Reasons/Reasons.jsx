c8f4908e5cf62cdb21364dcd5e23dc5900b2f84d
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-04-09 05:38:45 +0000

    modified changes

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -4,6 +4,9 @@ import Gym2 from '../../img/gym2.jpg'
 import Gym3 from '../../img/gym3.jpg'
 import Gym4 from '../../img/gym4.jpg'
 import Tick from '../../img/tick.png'
+import Adidas from '../../img/adidas.png'
+import Nike from '../../img/nike.png'
+
 
 
 const Reasons = () => {
@@ -22,22 +25,33 @@ const Reasons = () => {
             <span> Choose Us?</span>
         </div>
         <div className='details-r'>
+            <div>
+                <img src={Tick} alt="" />
+               <span>OVER 140+ EXPERT COACHES</span>
+            </div>
+            <div>
+                <img src={Tick} alt="" />
+               <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+            </div>
+            <div>
+                <img src={Tick} alt="" />
+                <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+            </div>
             
-                <img src={Tick} alt=""className='tick'></img>
-               <span></span>
-          
-                <img src={Tick} alt=""className='tick' />
-               <span></span>
-           
-            
-                <img src={Tick} alt="" className='tick'/>
-                <span></span>
-            
-            
-                <img src={Tick} alt="" className='tick'/>
-                <span></span>
+            <div>
+                <img src={Tick} alt="" />
+                <span>RELIABLE PARTNERS</span>
+            </div>
             
         </div>  
+        <span style={{color:'var(--gray)', fontWeight:"normal", paddingLeft:"2px"}}>
+          OUR PARTNERS
+        </span>
+        <div className='partners'>
+          <img src={Adidas} alt="" style={{width:"140px", height:"90px"}}/>
+          <img src={Nike} alt="" style={{ width:"140px", height:"90px"}}/>
+          <img src="" alt="" />
+        </div>
     </div>
   </div>   
   )
