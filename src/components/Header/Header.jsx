28571899912d1fa5e589fba30cb387d77bd708f3
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-04-17 20:07:08 +0000

    modified changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,28 @@
 import './Header.css'
 import Logo from '../../img/logo.png'
+import Bar from '../../img/bar.png'
+import { useState } from 'react'
+
 
 const Header = () => {
+const [menuOpen, setMenuOpen] = useState(false)
+  const mobile = window.innerWidth <= 768 ? true : false;
+
   return (
     <div className="header">
         <img src={Logo} alt="" className='logo'/>
-        <ul className='header-menu'>
-            <li>Home</li>
-            <li>Programs</li>
-            <li>Why Us</li>
-            <li>Plans</li>
-            <li>Testimonials</li>
-        </ul>
+        {(menuOpen ===false && mobile ===true )? (
+          <div style={{background:'var(--appColor)', padding:'0.1rem',height:'2.5rem', marginBottom:'11.5rem'}} ><img src={Bar} alt="" style={{width:'2.5rem', height:'2.5rem'}} onClick={()=>setMenuOpen(true)} /></div>
+       
+         ): <ul className='header-menu'>
+                <li onClick={()=>setMenuOpen()}>Home</li>
+                <li onClick={()=>setMenuOpen()}>Programs</li>
+                <li onClick={()=>setMenuOpen()}>Why Us</li>
+                <li onClick={()=>setMenuOpen()}>Plans</li>
+                <li onClick={()=>setMenuOpen()}>Testimonials</li>
+            </ul>
+        }
+        
     </div>
   )
 }
